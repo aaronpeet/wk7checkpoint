0642daacbd172327a326b4cbc3e0e7722c96d619
aaronpeet <aaronpeet@icloud.com>
2021-08-13 16:41:09 -0600

    adding getAll to bugscontroller and service

--- wk7checkpoint/server/controllers/BugsController.js
+++ wk7checkpoint/server/controllers/BugsController.js
@@ -6,9 +6,19 @@ export class BugsController extends BaseController {
   constructor() {
     super('api/bugs')
     this.router
+      .get('', this.getAll)
       // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const bugs = await bugsService.getAll(req.query)
+      res.send(bugs)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
--- wk7checkpoint/server/services/BugsService.js
+++ wk7checkpoint/server/services/BugsService.js
@@ -4,6 +4,11 @@ class BugsService {
     const bug = await dbContext.Bugs.create(body)
     return await dbContext.Bugs.findById(bug._id).populate('creator', 'name picture')
   }
+
+  async getAll(query = {}) {
+    const bugs = await dbContext.Bugs.findById(query).populate('creator', 'name picture')
+    return bugs
+  }
 }
 
 export const bugsService = new BugsService()
