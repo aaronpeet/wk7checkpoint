f845ffda2cb37b6c16087f3a8ba4cf1faf96a7f6
aaronpeet <aaronpeet@icloud.com>
2021-08-13 15:46:31 -0600

    initial push

--- wk7checkpoint/server/db/DbContext.js
+++ wk7checkpoint/server/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
+import { Bug as BugSchema } from '../models/Bug'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Bugs = mongoose.model('Bug', BugSchema);
 }
 
 export const dbContext = new DbContext()
