eea4aa2ab342c4179bf476f14e310d50f5a27593
aaronpeet <aaronpeet@icloud.com>
2021-08-14 15:11:52 -0600

    Most of my postman tests are passing for bugs

--- wk7checkpoint/server/db/DbContext.js
+++ wk7checkpoint/server/db/DbContext.js
@@ -2,12 +2,15 @@ import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
 import { Bug as BugSchema } from '../models/Bug'
+import { Note as NoteSchema } from '../models/Note'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 
   Bugs = mongoose.model('Bug', BugSchema);
+
+  Notes = mongoose.model('Note', NoteSchema)
 }
 
 export const dbContext = new DbContext()
